e302d7617f5ea411d6c1ad29cef0290323de6247
EituPaul <zitupaul@gmail.com>
2022-12-19 23:55:34 +0600

    navigation
    Signed-off-by: EituPaul <zitupaul@gmail.com>

--- app/src/main/java/com/example/fishclassification/HomeActivity.java
+++ app/src/main/java/com/example/fishclassification/HomeActivity.java
@@ -2,22 +2,77 @@ package com.example.fishclassification;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 
+import com.google.android.material.navigation.NavigationView;
 
-public class HomeActivity extends AppCompatActivity {
+
+public class HomeActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener {
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        toolbar = findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Finding Nemo");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navView);
+        View view = navigationView.inflateHeaderView(R.layout.drawer_header);
+        navigationView.setNavigationItemSelectedListener(this);
 
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()){
+            case R.id.home:
+                Intent ii1 = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(ii1);
+
+                break;
+            case R.id.profile:
+                Intent ii2 = new Intent(getApplicationContext(), Profile.class);
+                startActivity(ii2);
+                break;
+            case R.id.classify:
+                Intent ii3 = new Intent(getApplicationContext(), Classify_Fish.class);
+                startActivity(ii3);
+
+                break;
+            case R.id.location:
+                Intent ii4 = new Intent(getApplicationContext(), Location.class);
+                startActivity(ii4);
+
+                break;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()== android.R.id.home){
+            drawerLayout.openDrawer(GravityCompat.START);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
